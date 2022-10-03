d9017749d90ec1d960d4e78ff8d71c6e13f946d4
lynma01 <lynma01@gmail.com>
2022-10-02 21:38:12 -0400

    init with hello_amqp

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+import pika
+
+def hello_amqp():
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='',
+                        routing_key='hello',
+                        body='Hello World!')
+
+    print("[x] Sent 'Hello World!'")
+
+    connection.close()
+
+if __name__ == "__main__":
+    hello_amqp()
